d13c578d46f1aa76c4eefe33512ba052fe3e1540
r0uge <31421977+r0uge@users.noreply.github.com>
2025-05-13 17:56:56 -0300

    Update Log_AIP_Filter.ps1
    Correcciones

--- ps-log-azure-information-protection/Log_AIP_Filter.ps1
+++ ps-log-azure-information-protection/Log_AIP_Filter.ps1
@@ -2,4 +2,4 @@
 #Toma la entrada y filtra que contencta SensitivityLabelId y ApplicationName y que no contengan SVCAIPFS@tenaris.com.
 #Debido que tengo version PS5 el Select-String carece de -Raw pro lo que que se tiene que formatear la salida con ForEachObject
 
-Select-String -Path .\*.txt -Pattern "SensitivityLabelId" | Select-String -Path -Pattern "ApplicationName" | Select-String -Path -Pattern "SVCAIPFS@tenaris.com" -NotMatch | ForEach-Object { $_.Line } | Out-File -FilePath C:\Users\T23952\Documents\AIP_Log.txt
+Select-String -Path .\*.txt -Pattern "SensitivityLabelId" | Select-String -Path -Pattern "ApplicationName" | Select-String -Path -Pattern "Excluir_User" -NotMatch | ForEach-Object { $_.Line } | Out-File -FilePath X:\AIP_Log.txt
